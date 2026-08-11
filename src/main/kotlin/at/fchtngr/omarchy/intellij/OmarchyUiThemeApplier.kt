b6777609860c5be9fdeb845f4726c044bba56d93
Thomas Feichtinger <t.feichtinger@pm.me>
2026-08-11 14:26:18 +0200

    use UIThemeLookAndFeelInfoImpl for newer IDE compatibility
    UIThemeBasedLookAndFeelInfo was removed from newer IntelliJ versions,
    causing a NoClassDefFoundError when applying Omarchy themes. Replace it
    with UIThemeLookAndFeelInfoImpl, the class it delegated to.

--- src/main/kotlin/at/fchtngr/omarchy/intellij/OmarchyUiThemeApplier.kt
+++ src/main/kotlin/at/fchtngr/omarchy/intellij/OmarchyUiThemeApplier.kt
@@ -2,7 +2,7 @@ package at.fchtngr.omarchy.intellij
 
 import com.intellij.ide.ui.LafManager
 import com.intellij.ide.ui.UITheme
-import com.intellij.ide.ui.laf.UIThemeBasedLookAndFeelInfo
+import com.intellij.ide.ui.laf.UIThemeLookAndFeelInfoImpl
 import com.intellij.openapi.diagnostic.thisLogger
 import java.nio.file.Files
 
@@ -12,7 +12,7 @@ object OmarchyUiThemeApplier {
     fun apply(manifest: OmarchyThemePayload) {
         Files.newInputStream(OmarchyPaths.themeJson).use { input ->
             val theme = UITheme.Companion.loadTempThemeFromJson(input, manifest.name)
-            val laf = UIThemeBasedLookAndFeelInfo(theme)
+            val laf = UIThemeLookAndFeelInfoImpl(theme)
             val manager = LafManager.getInstance()
             manager.setCurrentUIThemeLookAndFeel(laf)
             manager.updateUI()
